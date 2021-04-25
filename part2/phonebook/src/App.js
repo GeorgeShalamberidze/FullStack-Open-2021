4e78c68ad9a0f43c5f16d9b6bf8bd00b129751a3
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2021-04-25 14:29:24 +0400

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,23 +2,32 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0 }
+    { name: 'Arto Hellas', id: 0, number: "955 555 555 555" }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    setNewName("")
     if (persons.some(p => p.name === newName)) {
       alert(`${newName} Already Exists`)
     }
-    else {
-      setPersons(persons.concat({ name: newName, id: persons[persons.length - 1].id + 1 }))
+    else if (!newNumber === "" && persons.some(p => p.number === newNumber)) {
+      alert(`${newNumber} Already Exists`)
     }
+    else if(!newNumber == "" || !newName == ""){
+      setPersons(persons.concat({ name: newName, id: persons[persons.length - 1].id + 1, number: newNumber }))
+    }
+    setNewName("")
+    setNewNumber('')
   }
 
   const handleChange = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -28,6 +37,13 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleChange} />
         </div>
+
+        <br />
+
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+
         <div>
           <button type="submit" onClick={handleSubmit}>add</button>
         </div>
@@ -35,7 +51,7 @@ const App = () => {
       <h2>Numbers</h2>
       {persons.map(person => {
         return (
-          <p key={person.id}>{person.name}</p>
+          <p key={person.id}>{person.name} {person.number}</p>
         )
       })}
     </div>
