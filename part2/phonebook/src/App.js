27991e438e1a7de08b1f8a4d72dddbf5bfc145b1
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2021-04-25 18:10:45 +0400

    Update App.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0, number: "955 555 555 555" }
+    { name: 'Arto Hellas', id: 0, number: 555555555 }
   ])
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState()
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [filter, setFilter] = useState([])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -15,19 +17,22 @@ const App = () => {
     else if (!newNumber === "" && persons.some(p => p.number === newNumber)) {
       alert(`${newNumber} Already Exists`)
     }
-    else if(!newNumber == "" || !newName == ""){
+    else if (!newNumber == "" || !newName == "") {
       setPersons(persons.concat({ name: newName, id: persons[persons.length - 1].id + 1, number: newNumber }))
     }
     setNewName("")
     setNewNumber('')
   }
 
-  const handleChange = (e) => {
-    setNewName(e.target.value)
-  }
+  const handleChange = (e) => setNewName(e.target.value)
+  const handleNumberChange = (e) => setNewNumber(e.target.value)
+  const handleSearch = (e) => setSearch(e.target.value)
 
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value)
+  const showSearch = () => {
+    if (persons.find(person => person.name.includes(search))) {
+      setFilter(filter.push({...persons}))
+      console.log(filter)
+    }
   }
 
   return (
@@ -35,13 +40,18 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={(e) => e.preventDefault()}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          <input placeholder="Search..." value={search} onChange={handleSearch} />
+          <button onClick={showSearch}>Search</button>
         </div>
+      </form>
+      <br />
 
-        <br />
-
+      <form onSubmit={(e) => e.preventDefault()}>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          name: <input value={newName} onChange={handleChange} placeholder="enter name..." />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} placeholder="enter number..." />
         </div>
 
         <div>
