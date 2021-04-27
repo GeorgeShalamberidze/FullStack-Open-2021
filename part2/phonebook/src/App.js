94a37b92ded00c40b1eba695585124f8661c6af9
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2021-04-27 23:48:43 +0400

    Implement search func

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0, number: 555555555 }
+    { name: 'Arto Hellas', id: 0, number: 555555555 },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
-  const [filter, setFilter] = useState([])
+  let filteredContacts = search == '' ? [] : persons.filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -28,24 +28,18 @@ const App = () => {
   const handleNumberChange = (e) => setNewNumber(e.target.value)
   const handleSearch = (e) => setSearch(e.target.value)
 
-  const showSearch = () => {
-    if (persons.find(person => person.name.includes(search))) {
-      setFilter(filter.push({...persons}))
-      console.log(filter)
-    }
-  }
-
   return (
+
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={(e) => e.preventDefault()}>
         <div>
           <input placeholder="Search..." value={search} onChange={handleSearch} />
-          <button onClick={showSearch}>Search</button>
         </div>
       </form>
-      <br />
 
+      <h2>Add new Contact</h2>
+      
       <form onSubmit={(e) => e.preventDefault()}>
         <div>
           name: <input value={newName} onChange={handleChange} placeholder="enter name..." />
@@ -62,6 +56,13 @@ const App = () => {
       {persons.map(person => {
         return (
           <p key={person.id}>{person.name} {person.number}</p>
+        )
+      })}
+
+      <h1>Filtered: </h1>
+      {filteredContacts.map(p => {
+        return (
+          <p key={p.id}>{p.name} {p.number}</p>
         )
       })}
     </div>
